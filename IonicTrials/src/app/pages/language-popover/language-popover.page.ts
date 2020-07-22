fc35e3179416149424e7e28a08ec37b0e0182cd2
ionictrialsuser5 <ionictrialsuser5@gmail.com>
2020-07-22 12:47:41 +0530

    changed service package

--- IonicTrials/src/app/pages/language-popover/language-popover.page.ts
+++ IonicTrials/src/app/pages/language-popover/language-popover.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PopoverController } from '@ionic/angular';
-import { LanguageService } from 'src/app/services/language.service';
+import { LanguageService } from 'src/app/core/language.service/language.service';
 
 @Component({
   selector: 'app-language-popover',
