b2315f06d01de7cc84671166539657953055262b
ionicTrialsUser3 <ionicTrialsUser3@gmail.com>
2020-08-05 13:29:57 +0530

    timer working

--- IonicTrials/src/app/timer-count-down/timer-count-down.page.ts
+++ IonicTrials/src/app/timer-count-down/timer-count-down.page.ts
@@ -9,23 +9,69 @@ import { CountdownComponent } from 'ngx-countdown';
 })
 export class TimerCountDownPage {
  
+//   interval;
+//   status = 'ready';
+//   @ViewChild('countdown') counter: CountdownComponent;
 
-  status = 'ready';
-  @ViewChild('countdown') counter: CountdownComponent;
+//   start() {
+//     this.interval = setInterval(() => {
+//     this.status = 'started';
+//     this.counter.begin();
+//     },)
+//   }
 
-  start() {
-    this.status = 'started';
-    this.counter.begin();
+//   finishTest() {
+//     console.log("count down", this.counter);
+//     setTimeout(() => this.counter.restart());
+//     this.status = 'restarted';
+//   }
+
+//   resetTimer() {
+//     this.counter.restart();
+//   }
+
+//   pauseTimer() {
+//     clearInterval(this.interval);
+//   }
+// }
+
+time: number = 0;
+  interval;
+
+  startTimer() {
+    console.log("=====>");
+    this.interval = setInterval(() => {
+      if (this.time === 0) {
+        this.time--;
+      } else {
+        this.time--;
+      }
+    }, 1000);
   }
-
-  finishTest() {
-    console.log("count down", this.counter);
-    setTimeout(() => this.counter.restart());
-    this.status = 'restarted';
+  pauseTimer() {
+    clearInterval(this.interval);
   }
 
   resetTimer() {
-    this.counter.restart();
+       //  this.counter.restart();
+      stop();
+       }
+
+
+  transform(value: number): string {
+    var sec_num = value; 
+
+  var days = Math.floor(sec_num / (3600 * 24));
+  var hours = Math.floor((sec_num - (days * (3600 * 24))) / 3600);
+  var minutes = Math.floor((sec_num - (days * (3600 * 24)) - (hours * 3600)) / 60);
+  var seconds = sec_num - (days * (3600 * 24)) - (hours * 3600) - (minutes * 60);
+
+  if (hours < 10) {hours = 0+hours;}
+  if (minutes < 10) {minutes = 0+minutes;}
+  if (seconds < 10) {seconds = 0+seconds;}
+  return hours+':'+minutes+':'+seconds;
+
+
   }
 }
 
