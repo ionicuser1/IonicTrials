2e2978b557d07e4ade3f2113c6fea9a68ec37593
ionicTrialsUser3 <ionicTrialsUser3@gmail.com>
2020-08-07 12:01:02 +0530

    notification

---
+++ IonicTrials/src/app/notification3/notification3.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notification3',
+  templateUrl: './notification3.component.html',
+  styleUrls: ['./notification3.component.scss'],
+})
+export class Notification3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
